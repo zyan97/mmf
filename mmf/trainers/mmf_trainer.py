4bb2cb1e3f78b31d3d87b363cb35cc7a2b8fba6b
zyan97 <55512807+zyan97@users.noreply.github.com>
2020-11-13 10:05:21 +0800

    Update mmf_trainer.py

--- mmf/trainers/mmf_trainer.py
+++ mmf/trainers/mmf_trainer.py
@@ -84,10 +84,12 @@ class MMFTrainer(
         model_list = ['multi_task_pair_wise_visual_bert', 'attn_based_multi_task_pair_wise_visual_bert', 'attn_based_pair_concat_visual_bert']
         if self.config.model in model_list:
             attributes = self.config.model_config['visual_bert']
-        # Easy way to point to config for other model
-        if isinstance(attributes, str):
-            attributes = self.config.model_config[attributes]
-
+        else:
+          attributes = self.config.model_config[self.config.model]
+          # Easy way to point to config for other model
+          if isinstance(attributes, str):
+              attributes = self.config.model_config[attributes]
+        
         with omegaconf.open_dict(attributes):
             attributes.model = self.config.model
 
